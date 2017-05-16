83d088c10eaa9f4c1574552038987418deb5a1fe
曹麟 <caol@youlijinfu.com>
2017-05-16 13:18:08 +0800

    init

---
+++ Quartz.Topshelf.Integration/Quartz.Topshelf.Integration/HostHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace Quartz.Topshelf.Integration
+{
+    /// <summary>
+    /// 启动服务类
+    /// </summary>
+    public class HostHelper
+    {
+        public void Start(string serviceName, string displayName = null, string description = null)
+        {
+            if (string.IsNullOrEmpty(serviceName))
+            {
+                throw new Exception("错误：未定义服务名称！需定义有效的服务名称");
+            }
+            if (string.IsNullOrEmpty(displayName))
+            {
+                serviceName = displayName;
+            }
+            if (string.IsNullOrEmpty(description))
+            {
+                description = displayName;
+            }
+
+            HostFactory.Run(x =>
+            {
+                x.Service<QuartzService>(s =>
+                {
+                    s.ConstructUsing(name => new QuartzService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.RunAsLocalSystem();
+
+                x.SetDescription(description);
+                x.SetDisplayName(displayName);
+                x.SetServiceName(serviceName);
+            });
+        }
+    }
+}
---
+++ Quartz.Topshelf.Integration/Quartz.Topshelf.Integration/QuartzService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using Common.Logging.Simple;
+using Quartz.Impl;
+
+namespace Quartz.Topshelf.Integration
+{
+    /// <summary>
+    /// Quartz启动类
+    /// </summary>
+    public class QuartzService
+    {
+        private readonly IScheduler _scheduler;
+        private static readonly NameValueCollection Config = new NameValueCollection
+        {
+            {"quartz.plugin.xml.fileNames", "~/quartz_jobs.xml"},
+            {"quartz.plugin.xml.type", "Quartz.Plugin.Xml.XMLSchedulingDataProcessorPlugin, Quartz"}
+        };
+
+        public QuartzService() : this(Config)
+        {
+
+        }
+
+        public QuartzService(NameValueCollection config)
+        {
+            LogManager.Adapter = new ConsoleOutLoggerFactoryAdapter { Level = LogLevel.Info };
+            var factory = new StdSchedulerFactory(config);
+            _scheduler = factory.GetScheduler();
+        }
+
+        public void Start()
+        {
+            _scheduler.Start();
+        }
+
+        public void Stop()
+        {
+            _scheduler.Shutdown();
+        }
+    }
+
+}
